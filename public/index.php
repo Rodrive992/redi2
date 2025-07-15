ed73ac7712a4a95fc292ca3e1710c5b74228e088
Rodri <rodrigovegga992@gmail.com>
2025-07-15 16:08:11 -0300

    Agregamos variables en el archivo index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set('upload_max_filesize', '100M');
+ini_set('post_max_size', '100M');
+ini_set('memory_limit', '256M');
+ini_set('max_execution_time', '300'); // Opcional si tu carga es pesada
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
